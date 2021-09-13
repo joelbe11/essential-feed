b6d0282dfba186f2274206299d5837c159b07542
joelbe11 <joelbell@me.com>
2021-09-13 07:05:34 -0400

    Guarantee we do not deliver a result (invoke the completion block) after the `RemoteFeedLoader` instance has been deallocated

--- EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -121,6 +121,20 @@ class RemoteFeedLoaderTests: XCTestCase {
             client.complete(withStatusCode: 200, data: json)
         })
     }
+    
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url: URL = .init(string: "http://any-url.com")!
+        let client: HTTPClientSpy = .init()
+        var sut: RemoteFeedLoader? = .init(url: url, client: client)
+        
+        var capturedResults: [RemoteFeedLoader.Result] = []
+        sut?.load { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
 
     // MARK: - Helpers
     
