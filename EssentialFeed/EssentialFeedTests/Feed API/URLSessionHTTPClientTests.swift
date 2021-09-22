5e730cde6867d55e58c5c458e88f09e2a0fb9a15
Joel <joelbell@me.com>
2021-09-22 07:18:22 -0400

    Networking -  Identifying, Debugging, and Solving Data Races with the Thread Sanitizer (#12)
    * Finish URL loading when observing requests to make sure all URL requests are finished before the test method returns. This way, we prevent data races with threads living longer than the test method that initiated them.
    
    * Increase timeout for end-to-end tests

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -42,7 +42,6 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override class func canInit(with request: URLRequest) -> Bool {
-            requestObserver?(request)
             return true
         }
         
@@ -51,6 +50,11 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
+            if let requestObserver = URLProtocolStub.requestObserver {
+                client?.urlProtocolDidFinishLoading(self)
+                return requestObserver(request)
+            }
+            
             if let data = Self.stub?.data {
                 client?.urlProtocol(self, didLoad: data)
             }
